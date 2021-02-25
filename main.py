821483f4fc90949121a1c7d3fb6f9b8f9a587507
das1292 <jakub.klos.10@wp.pl>
2021-02-25 11:08:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from math import *
+#Zad1
+# a='napis'
+# b=1
+# print(a)
+# print(b)
+#Zad2
+# a= 2
+# b= 4
+# dodawanie= a+b
+# odejmowanie= a-b
+# dzielenie= b/a
+# dzielenie_calkowite= b//a
+# mnozenie= a*b
+# potegowanie= a**3
+# reszta= b%3
+# print(sqrt(b))
+# print(dodawanie)
+# print(odejmowanie)
+# print(dzielenie)
+# print(dzielenie_calkowite)
+# print(mnozenie)
+# print(potegowanie)
+# print(reszta)
+#Zad3
+# a= 8
+# b= 8
+# c= 8
+# d= 8
+# e= 8
+# f= 8
+# a+=2
+# b-=2
+# c/=2
+# d*=10
+# e**=2
+# f%=5
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+#Zad4
+e= exp(1)
+e**=10
+print(e)
+x=(log(5 + sin(8)**2))
+c=pow(x,1/6)
+print(c)
+print (min(3,55))
+print (max(4,80))
